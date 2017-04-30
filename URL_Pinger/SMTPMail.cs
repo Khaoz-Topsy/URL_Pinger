b27cc295feb66acf1035467260bd093b30bb47e9
kurt.lourens <kurt.lourens@entelect.co.za>
2017-05-01 01:04:33 +0200

    First Commit

---
+++ URL_Pinger/SMTPMail.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace URL_Pinger
+{
+    class SMTPMail
+    {
+        public static void Send(string Subject, string Message)
+        {
+            new Thread(() =>
+            {
+                try
+                {
+                    Thread.CurrentThread.IsBackground = true;
+
+                    string Email = SecretData.ToEmailAddress;
+                    string SenderEmail = SecretData.FromEmailAddress;
+                    string Password = SecretData.Password;
+
+                    var smtp = new SmtpClient
+                    {
+                        Host = "smtp.gmail.com",
+                        Port = 587,
+                        EnableSsl = true,
+                        DeliveryMethod = SmtpDeliveryMethod.Network,
+                        UseDefaultCredentials = false,
+                        Credentials = new NetworkCredential(SenderEmail, Password)
+                    };
+
+                    using (var message = new MailMessage(SenderEmail, Email) { Subject = Subject, Body = Message })
+                    {
+                        smtp.Send(message);
+                    }
+                }
+                catch (Exception) { }
+
+            }).Start();
+        }
+    }
+}
---
+++ URL_Pinger/SecretData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URL_Pinger
+{
+    public class SecretData
+    {
+        public static string ToEmailAddress = "";
+        public static string FromEmailAddress = "";
+        public static string Password = "";
+    }
+}
